15aa0d3c2d7dea1f8aeddc8f6f30a427bdce7401
SvetlanaForte-2000 <svitlanamikheieva@gmail.com>
2025-07-23 10:53:20 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const reversedStr = String(Math.abs(n)).split('').reverse().join('');
+  return Number(reversedStr);
 };
